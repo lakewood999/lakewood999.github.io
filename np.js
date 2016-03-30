7d4e3880977e28b8e351a9079941e547e08c13ea
Steven <lakewood999@gmail.com>
2016-03-30 12:24:19 -0400

    Update np.js

--- np.js
+++ np.js
@@ -1,5 +1,20 @@
+/*
+Template: 
+
+function name() {
+  var doc = document.getElementById("np")
+  doc.innerHTML = "name";
+}
+
+*/
+
 function placeholder() {
   //alert("Working");
   var doc = document.getElementById("np")
   doc.innerHTML = "Test";
+}
+
+function stop() {
+  var doc = document.getElementById("np")
+  doc.innerHTML = "N/A";
 }
