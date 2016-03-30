a0143e3c29a7bd6033a467af5aa89b929e4393f4
Steven <lakewood999@gmail.com>
2016-03-30 12:34:46 -0400

    Update np.js

--- np.js
+++ np.js
@@ -18,3 +18,47 @@ function stop() {
   var doc = document.getElementById("np")
   doc.innerHTML = "N/A";
 }
+
+function RRRYB() {
+  var doc = document.getElementById("np")
+  doc.innerHTML = "Row Row Row You Boat(Complicated)";
+}
+
+function DYKTMM() {
+  var doc = document.getElementById("np")
+  doc.innerHTML = "Do You Know The Muffin Man(Complicated)";
+}
+
+function Poteo() {
+  var doc = document.getElementById("np")
+  doc.innerHTML = "Protectors of the Earth(Original)";
+}
+
+function Potee() {
+  var doc = document.getElementById("np")
+  doc.innerHTML = "Protecotrs of the Earth(Extended)";
+}
+
+function Fotso() {
+  var doc = document.getElementById("np")
+  doc.innerHTML = "Flight of the Silverbird(Original)";
+}
+
+function Fotse() {
+  var doc = document.getElementById("np")
+  doc.innerHTML = "Flight of the Silverbird(Extended)";
+}
+
+function PoteP() {
+  var doc = document.getElementById("np")
+  doc.innerHTML = "Protectors of the Earth Piano";
+}
+
+function FotsP() {
+  var doc = document.getElementById("np")
+  doc.innerHTML = "Flight of the Silverbird Piano";
+}
+
+
+
+
